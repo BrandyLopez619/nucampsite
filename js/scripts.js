547b2587fa6e2b91f92f8e37ca82a5d79193da82
Brandy Lopez <brandylopez@myobra.com>
2022-07-30 11:13:02 -0700

    Week 4 Assingment

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
-        }
-    });
+$(function () {
+  $(".carousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
 });
 
+// $("#loginButton").on('click',function(){
+//     $('#loginModal').modal('show');
+
+// });
+$(function () {
+  $("#loginButton").on("click", function () {
+    $("#loginModal").modal({
+      keyboard: false, //
+    });
+  });
+});
+
+$(function () {
+  $("#reserveButton").on("click", function () {
+    $("#reserveModal").modal({
+      keyboad: false,
+    });
+  });
+});
