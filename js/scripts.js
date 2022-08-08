aea7328b87801d6a760204c5c180f8743cec3191
Brandy Lopez <brandylopez@myobra.com>
2022-08-07 22:21:22 -0700

    final workshop wk 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,36 @@
 $(function () {
-  $(".carousel").carousel({ interval: 2000 });
-  $("#carouselButton").click(function () {
-    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-      $(".carousel").carousel("pause");
-      $("#carouselButton").children("i").removeClass("fa-pause");
-      $("#carouselButton").children("i").addClass("fa-play");
-    } else {
-      $(".carousel").carousel("cycle");
-      $("#carouselButton").children("i").removeClass("fa-play");
-      $("#carouselButton").children("i").addClass("fa-pause");
-    }
-  });
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
 });
 
+$(function () {
 // $("#loginButton").on('click',function(){
 //     $('#loginModal').modal('show');
 
 // });
+
 $(function () {
-  $("#loginButton").on("click", function () {
-    $("#loginModal").modal({
-      keyboard: false, //
+    $("#loginButton").on("click", function () {
+        $("#loginModal").modal({
+            keyboard: false, //
+        });
     });
-  });
 });
 
 $(function () {
-  $("#reserveButton").on("click", function () {
-    $("#reserveModal").modal({
-      keyboad: false,
+    $("#reserveButton").on("click", function () {
+        $("#reserveModal").modal({
+            keyboad: false,
+        });
     });
-  });
 });
